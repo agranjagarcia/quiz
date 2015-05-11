7a39c860304025e722f0b302ac0ab5f0fa3de747
javier-diezmart <javier10mart@hotmail.com>
2015-05-11 13:26:31 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-// Página de Entrada 
+// Página de Entrada
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
