e6528747968249003489432dd5d26e54bbcf5b7b
Alejandro Granja Garcia <a.granja@alumnos.upm.es>
2015-05-27 01:29:09 +0200

    Subir imágenes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
@@ -33,10 +34,10 @@ router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, userContr
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new',                  sessionController.loginRequired, quizController.new);
+router.get('/quizes/new',                  sessionController.loginRequired, multer({ dest: './public/media/'}), quizController.new);
 router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.ownershipRequired, quizController.update);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.ownershipRequired, multer({ dest: './public/media/'}), quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
